ce0c2e864eef52ada4062627c4e8650d9f47b5fb
Siang Peng Ng <siangpeng@Siangs-MacBook-Pro.local>
2023-09-13 11:22:46 +0800

    end of section 2

---
+++ src/AuctionService/Data/MigrationPatch/20230911091714_FirstPatch.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.MigrationPatch
+{
+    /// <inheritdoc />
+    public partial class FirstPatch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
