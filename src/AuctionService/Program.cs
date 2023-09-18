98ee02ef7a05b16c1043e34753975e8522e0beb1
Siang Peng Ng <siangpeng@Siangs-MacBook-Pro.local>
2023-09-18 14:21:24 +0800

    end of section 5

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -2,6 +2,7 @@ using AuctionService;
 using AuctionService.Data;
 using AuctionService.Entities;
 using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
@@ -33,11 +34,21 @@ builder.Services.AddMassTransit(x => {
     });
 });
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => {
+        options.Authority = builder.Configuration["IdentityServiceUrl"]; // will talk to server
+        options.RequireHttpsMetadata = false; // running on http now
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseAuthentication(); // call this before auth, do the jwt thingly
 app.UseAuthorization();
+
 
 app.MapControllers();
 
